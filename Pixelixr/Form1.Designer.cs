894e0b9959cde01bfb7b8200d9a18b5585b0a92a
WiseClock <kuanghao04@gmail.com>
2016-02-22 21:20:36 -0800

    initial commit.

---
+++ Pixelixr/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Pixelixr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btn_shrink = new System.Windows.Forms.Button();
+            this.btn_ascii = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.maxWidth = new System.Windows.Forms.NumericUpDown();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtSeparator = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.maxWidth)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(3, 152);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(149, 50);
+            this.btn_browse.TabIndex = 0;
+            this.btn_browse.Text = "Browse";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Pixelixr.Properties.Resources.grid;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 143);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::Pixelixr.Properties.Resources.grid;
+            this.pictureBox2.Location = new System.Drawing.Point(72, 208);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btn_shrink
+            // 
+            this.btn_shrink.Location = new System.Drawing.Point(3, 236);
+            this.btn_shrink.Name = "btn_shrink";
+            this.btn_shrink.Size = new System.Drawing.Size(63, 50);
+            this.btn_shrink.TabIndex = 3;
+            this.btn_shrink.Text = "Shrink";
+            this.btn_shrink.UseVisualStyleBackColor = true;
+            this.btn_shrink.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_ascii
+            // 
+            this.btn_ascii.Location = new System.Drawing.Point(3, 318);
+            this.btn_ascii.Name = "btn_ascii";
+            this.btn_ascii.Size = new System.Drawing.Size(149, 50);
+            this.btn_ascii.TabIndex = 5;
+            this.btn_ascii.Text = "Pixelate";
+            this.btn_ascii.UseVisualStyleBackColor = true;
+            this.btn_ascii.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtSeparator);
+            this.panel2.Controls.Add(this.maxWidth);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.btn_ascii);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.btn_browse);
+            this.panel2.Controls.Add(this.btn_shrink);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(158, 511);
+            this.panel2.TabIndex = 7;
+            // 
+            // maxWidth
+            // 
+            this.maxWidth.Location = new System.Drawing.Point(4, 210);
+            this.maxWidth.Maximum = new decimal(new int[] {
+            80,
+            0,
+            0,
+            0});
+            this.maxWidth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.maxWidth.Name = "maxWidth";
+            this.maxWidth.Size = new System.Drawing.Size(62, 20);
+            this.maxWidth.TabIndex = 6;
+            this.maxWidth.Value = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.richTextBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(158, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(417, 511);
+            this.panel3.TabIndex = 8;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(417, 511);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // txtSeparator
+            // 
+            this.txtSeparator.Location = new System.Drawing.Point(4, 292);
+            this.txtSeparator.Name = "txtSeparator";
+            this.txtSeparator.Size = new System.Drawing.Size(148, 20);
+            this.txtSeparator.TabIndex = 7;
+            this.txtSeparator.Text = "\\\\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 511);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Pixelixr";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.maxWidth)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btn_shrink;
+        private System.Windows.Forms.Button btn_ascii;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.NumericUpDown maxWidth;
+        private System.Windows.Forms.TextBox txtSeparator;
+    }
+}
+
---
+++ Pixelixr/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pixelixr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fdl = new OpenFileDialog();
+            if (fdl.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var filename = fdl.FileName;
+                pictureBox1.Image = Image.FromFile(filename);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+                return;
+
+            Image i = pictureBox1.Image;
+            double width = i.Width;
+            double height = i.Height;
+            Image result = null;
+
+            double max = (double)maxWidth.Value;
+
+            /*
+            if (width > max && height > max)
+            {
+                if (width > max)
+                {
+                    height *= max / width;
+                    width = max;
+                }
+                else
+                {
+                    width *= max / height;
+                    height = max;
+                }
+                Bitmap b = ResizeImage(i, (int)width, (int)(height / 1.82));
+                result = b;
+            }
+            else
+                result = (Image)i.Clone();
+             */
+
+            if (width > max)
+            {
+                height *= max / width;
+                width = max;
+            }
+            result = ResizeImage(i, (int)width, (int)(height / 1.82));
+
+            pictureBox2.Image = result;
+        }
+
+        private static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image == null)
+                return;
+            Thread t = new Thread(a);
+            t.Start();
+        }
+
+        private void a()
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                richTextBox1.Text = "";
+            });
+            Bitmap img = (Bitmap)pictureBox2.Image;
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < img.Height; i++)
+            {
+                for (int j = 0; j < img.Width; j++)
+                {
+                    Color c = img.GetPixel(j, i);
+                    if (c.A == 0)
+                        sb.Append(" ");
+                    else
+                        sb.Append("[[g;#" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2") + ";]#]");
+                }
+                sb.Append(Regex.Unescape(txtSeparator.Text));
+            }
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                richTextBox1.Text = sb.ToString();
+                richTextBox1.Focus();
+                richTextBox1.SelectAll();
+            });
+        }
+    }
+}
